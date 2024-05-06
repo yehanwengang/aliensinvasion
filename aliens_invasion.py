f2b4bf58f3d670e25acf56e3de5dfd97f6fdf4ff
yehanwengang <tanwengang@126.com>
2024-05-06 10:31:40 +0800

    更新

--- aliens_invasion.py
+++ aliens_invasion.py
@@ -5,3 +5,31 @@
 @Project:aliensinvasion
 @IDE:PyCharm
 """
+
+import sys
+import pygame
+
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1440, 900))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            # 侦听键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
